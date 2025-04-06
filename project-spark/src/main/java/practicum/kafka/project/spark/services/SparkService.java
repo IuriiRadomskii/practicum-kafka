ce667c6ce2cc23baa76b41021491705ab8ab1859
y.radomskiy <y.radomskiy@dreamkas.ru>
2025-04-06 18:27:36 +0500

    project: spark  client is compatible with container spark

--- project-spark/src/main/java/practicum/kafka/project/spark/services/SparkService.java
+++ project-spark/src/main/java/practicum/kafka/project/spark/services/SparkService.java
@@ -1,8 +1,10 @@
 package practicum.kafka.project.spark.services;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,7 +15,9 @@ public class SparkService {
 
     static Logger log = LoggerFactory.getLogger(SparkService.class);
 
-    private static String HDFS_FILE_PATH = "hdfs://localhost:9000/data/client_requests.txt";
+    private static String SPARK = "spark://localhost:7077";
+    private static String HDFS = "hdfs://localhost:9000";
+    private static String HDFS_FILE_PATH = HDFS + "/data/client_requests.txt";
 
     public static void main(String[] args) {
         try {
@@ -25,9 +29,15 @@ public class SparkService {
 
     public static void processHdfsFile() {
         log.info("Spark processing HDFS file started");
-        SparkConf sparkConf = new SparkConf()
-                .setAppName("KafkaHdfsSparkConsumer")
-                .setMaster("spark://localhost:7077");
+        SparkConf sparkConf =
+                new SparkConf()
+                        .setAppName("spark-application")
+                        .setMaster(SPARK)
+                        .set("spark.hadoop.fs.default.name", HDFS)
+                        .set("spark.hadoop.fs.defaultFS", HDFS)
+                        .set("spark.hadoop.fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName())
+                        .set("spark.hadoop.fs.hdfs.server", org.apache.hadoop.hdfs.server.namenode.NameNode.class.getName())
+                        .set("spark.hadoop.conf", org.apache.hadoop.hdfs.HdfsConfiguration.class.getName());
 
         try (JavaSparkContext sc = new JavaSparkContext(sparkConf)) {
             JavaRDD<String> hdfsData = sc.textFile(HDFS_FILE_PATH);
